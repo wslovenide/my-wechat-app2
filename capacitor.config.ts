b59a4fd82c6e24a04a3b4681af96d7c1ebe9d553
mac <mac@192.168.0.102>
2026-01-18 21:14:00 +0800

    1

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.weconnect.app',
   appName: '微连 WeConnect',
-  webDir: '.',
+  webDir: 'dist',
   server: {
     androidScheme: 'https',
     cleartext: true
